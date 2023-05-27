05956a64b2789ffefa1e325c69112937f78d8dd5
MikhailPL <muke.jry@gmail.com>
2023-05-27 16:16:33 +0300

    Oh, what awaits us 2

---
+++ model.py
@@ -0,0 +1,11 @@
+# Функция вывода данных записанных в файле
+def get_data():
+    return None
+
+# Запись нового контакта в файл
+def add_contact():
+    return None
+
+# Поиск контакта в файле
+def search():
+    return None
